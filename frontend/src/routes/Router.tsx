33061e67509e1a972778ffb706c7ad744fa01704
volka09 <upswing.squalls.6s@icloud.com>
2025-12-06 17:46:09 +0300

    Router additions

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,11 +1,19 @@
-// src/routes/Router.tsx
 import { createBrowserRouter } from "react-router-dom";
 import Home from "@pages/Home.tsx";
 import Article from "@pages/Article.tsx";
+import Category from "@pages/Category.tsx";
+import Login from "@pages/Login.tsx";
+import Register from "@pages/Register.tsx";
+import EditorArticleForm from "@pages/EditorArticleForm.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
+  { path: "/category/:slug", element: <Category /> },
   { path: "/article/:slug", element: <Article /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/editor/article", element: <EditorArticleForm /> },
+  { path: "/editor/article/:slug", element: <EditorArticleForm /> },
 ]);
 
 export default router;
